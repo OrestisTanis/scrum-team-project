aa67e36d86f8da888d16d8b3b3719a5cab7140ea
Orestis Tanis <tanisorestis@yahoo.com>
2020-04-29 15:19:51 +0300

    added models

---
+++ src/java/models/QuestionAnswers.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.List;
+
+/**
+ *
+ * @author Walter
+ */
+public class QuestionAnswers {
+    Question question;
+    List<Answer> answers;
+}
